38d79903985bea6a33ecd1245b39abaf090d56f7
dinhtrung1605 <127390593+dinhtrungndt@users.noreply.github.com>
2023-10-08 10:52:42 +0700

    up load trending now

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import useCurrentUser from "@/hooks/useCurrentUser";
 
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -24,7 +26,7 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
+  const { data: movies = [] } = useMovieList();
 
   return (
     <>
@@ -37,6 +39,9 @@ export default function Home() {
       <main>
         <Navbar />
         <Billboard />
+        <div className="pb-40">
+          <MovieList title="Trending Now" data={movies} />
+        </div>
       </main>
     </>
   );
